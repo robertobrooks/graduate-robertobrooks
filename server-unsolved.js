4e07b1c066fb741c2aed8e4eeb3eb3f634432e58
Rob Brooks <brooksaguirre@g.harvard.edu>
2019-05-06 22:17:07 -0500

    added solved/unsolved sets

--- server-unsolved.js
+++ server-unsolved.js
@@ -7,7 +7,7 @@
 //*************************************
 
 //*** 1. Require the 'inquirer' npm
-const inquirer = require('inquirer');
+//***ANSWER HERE
 
 //We create an array to store our date
 var myArray = [];
@@ -17,39 +17,18 @@ console.log('***********************************');
 console.log('*** Welcome to you TO-DO list *****');
 console.log('***********************************');
 
-//*** 2. Create an array to store username and password (remember to hide the password characters) using the right input
+//*** 2. Create an array called 'login' to store username and password (remember to hide the password characters) using the right input
 var login = [
-    {
-      type: 'input',
-      name: 'username',
-      message: "What's your username?",
- 
-//*** 2.5 Add a regular expresion to accept only letter and numers (OPTIONAL)
-      validate: function(value) {
-        var validUser = value.match(
-          /^[a-zA-Z0-9_.-]*$/i
-        );
-        if (validUser) {
-          return true;
-        }
-        return 'Please use letters and numbers only';
-      }
-    },
-    {
-      type: 'password',
-      name: 'password',
-      message: 'What is your password?',
-    }
-  ];
+//***ANSWER HERE
+]
 
-//*** 3. Create a second array to store the task
+//*** 3. Create a second array to store the task (the confirmation part of the array is given)
 var toDoList = [
   {
-    type: 'input',
-    name: 'task',
-    message: "Please type a task: "
+//***ANSWER HERE
   },
   {
+//This information will be given to the student
     type: 'confirm',
     name: 'moreTasks',
     message: 'Would you like to add another task?',
@@ -60,16 +39,17 @@ var toDoList = [
 //We will just prompt the username/password without verification -- for practice purposes
 function loginFunction() {
     inquirer.prompt(login).then(function(login) {
-//We will call getTasks recursively
+ //We will call getTasks recursively
         function getTasks() {
+            
             inquirer.prompt(toDoList).then(answers => {
 //4. Store the answers in MyArray
-              myArray.push(answers.task);
+// ***ANSWER HERE
               if (answers.moreTasks) {
                 getTasks();
               } else {
 //5. Display the results using console log and showing the stored data of myArray separated with commas
-                console.log('Your tasks are:', myArray.join(', '));
+//***ANSWER HERE
               }
             });
           }       
